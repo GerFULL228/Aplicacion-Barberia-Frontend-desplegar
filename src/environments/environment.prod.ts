19c77ee412d063a7ef01ce3544654b7a069cf6c6
Fernando-Ontt <u23227125@utp.edu.pe>
2026-07-13 23:49:14 -0500

    Configuración de envios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://aplicacion-barberia-backend-desplegar-production.up.railway.app/api/v1',
-  apiBaseUrl: 'https://aplicacion-barberia-backend-desplegar-production.up.railway.app/',
-  googleClientId: '201569536157-1g4431eka4lbfl5oepc11pjv4nkq3jch.apps.googleusercontent.com'
+    production: false,
+    apiUrl: 'http://localhost:8081/api/v1',
+    apiBaseUrl: 'http://localhost:8081/',
+    googleClientId: '201569536157-1g4431eka4lbfl5oepc11pjv4nkq3jch.apps.googleusercontent.com'
 };
